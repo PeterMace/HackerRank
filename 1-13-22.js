c9ddb54a9eb95d143b09360cdb45f7ebfbfd380b
Peter Mace <peterjmace@gmail.com>
2022-01-13 20:47:05 -0500

    caesarCipher restructured with comments

--- 1-13-22.js
+++ 1-13-22.js
@@ -46,3 +46,44 @@ function caesarCipher(s,  k) {
     }
     return result;
 }
+
+
+/*
+ * Complete the 'caesarCipher' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. STRING s
+ *  2. INTEGER k
+ */
+
+function caesarCipher(s,  k) {
+    let alphabet = "abcdefghijklmnopqrstuvwxyz";
+    alphabet = alphabet.split("");
+    let result = "";
+    
+    for(let char of s){
+        let letter = char.toLowerCase();
+        //If lowercase letter is not found in alphabet
+        if(alphabet.indexOf(letter) === -1){
+            //add character back to result
+            result += char; 
+        }else{
+            //find new letter at new location
+            let position = alphabet.indexOf(letter) + k;
+            if(position > 25){
+                position = position % 25 - 1 ;
+            }
+            //If original was capitalized, add capitalized version to result
+            if(char === char.toUpperCase()){
+                result += alphabet[position].toUpperCase();
+            }else{
+                result += alphabet[position];
+            }
+            
+        }
+    }
+    
+    return result;
+}
+
