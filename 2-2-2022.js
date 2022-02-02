43fa228b3bff39d0693fe772f2689f3b2e4aefe2
Peter Mace <peterjmace@gmail.com>
2022-02-02 13:52:44 -0500

    Brix hackerRank 2.Balancing Parantheses

--- 2-2-2022.js
+++ 2-2-2022.js
@@ -12,3 +12,30 @@ function isPrime(n) {
         return 1;
     }
 }
+
+
+ * Complete the 'getMin' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
+function getMin(s) {
+    const queue = [];
+    let added = 0;
+    for(let char of s){
+        if(char == "("){
+            queue.push(char);    
+        }else{
+            //If there is a paranthesis closing, remove one from queue
+            if(queue.length > 0){
+                queue.pop();
+            //else there is a paranthesis to add
+            }else{
+                added++;
+            }
+        }
+        
+    }
+    return added + queue.length;
+}
