0413a121b65fab27f3102c1ec13208d0f8cc3d9e
Petermace <peterjmace@gmail.com>
2022-05-02 13:40:39 -0400

    comments added to 905

--- 5-2-22.js
+++ 5-2-22.js
@@ -1,5 +1,13 @@
+#905. Sort Array By Parity
+
+
+
 var sortArrayByParity = function(nums) {
     nums = nums.sort((a, b) => 
      a % 2 - b % 2 )
     return nums;
 };
+
+
+//Runtime: 81 ms, faster than 80.49% of JavaScript online submissions for Sort Array By Parity.
+Memory Usage: 45 MB, less than 29.02% of JavaScript online submissions for Sort Array By Parity.
